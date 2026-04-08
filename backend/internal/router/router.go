5baa872dc99ef2eb8fd2211142bf47b968107424
Maria Luna <mlunat20@gmail.com>
2026-04-07 19:59:00 -0500

    chore: impl service call cloud vision api

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -3,11 +3,11 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/marialuna/prueba_tecnica/ai-image-analyzer/backend/internal/handler"
+	"github.com/marialuna/prueba_tecnica/ai-image-analyzer/backend/internal/usecase"
 )
 
-
-func SetupRouter() *gin.Engine {
+func SetupRouter(analyzeUseCase usecase.AnalyzeImageUseCase, maxImageSize int64) *gin.Engine {
 	r := gin.Default()
-	r.POST("/api/analyze", handler.AnalyzeHandler)
+	r.POST("/api/analyze", handler.AnalyzeHandler(analyzeUseCase, maxImageSize))
 	return r
 }
